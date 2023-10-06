25d65829ca0aa2befbfadaa005b762bfbdfec5c6
chinmayGhadigaonkar <ghadigaonkarchinmay@gmail.com>
2023-10-06 12:14:15 +0530

    Revert "Revert "filter working""
    This reverts commit b47bb3098d652f5e000de3aba1e30dbf8dc50ea5.

---
+++ src/utils/BootstrapButton.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+
+import { purple } from "@mui/material/colors";
+
+export const ColorButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText(purple[500]),
+  textTransform: 'none',
+  fontSize: 16,
+  backgroundColor: purple[500],
+  fontFamily:['sans-serif'],
+  "&:hover": {
+    backgroundColor: purple[700],
+  },
+}));
